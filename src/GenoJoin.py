c66ad0b2987412b079c77fab56ca07fa31369132
Moonipur <moo_sutthittha@hotmail.com>
2025-12-11 19:57:14 +0700

    Reject region that does Not have variants

--- src/GenoJoin.py
+++ src/GenoJoin.py
@@ -34,10 +34,11 @@ def track_usage(pids, stop_flag, result_dict):
     result_dict["max_mem"] = max_mem
 
 
-def main(setList):
+def main(setList, logger):
     n = len(setList)
     with tqdm(total=n, miniters=n // 10, desc="Processing") as pbar:
         for item in setList:
+            st = time()
 
             (
                 inputdir,
@@ -51,31 +52,37 @@ def main(setList):
                 gq,
             ) = item
 
-            print("\n")
-            print(f"[Region] {chromosome}\t{pos_start}\t{pos_end}")
-
             # Extract
             all_ref, all_var, uniq_var, samples = Extract_region(
                 inputdir, chromosome, pos_start, pos_end, qual, depth, gq
             )
 
-            SummaryVariants(all_var, uniq_var, samples)
+            if len(uniq_var) == 0:
+                pbar.update(1)
+                continue
 
             # Create DB
             w = WordDatabase(uniq_var, f"{chromosome}_{pos_start}_{pos_end}", dbdir)
             w.CreateDB(threads=1)
 
             # Map DB
-            s_time = time()
             for sample_idx, sample in enumerate(samples):
                 sentence_var = [var[:2] for var in all_var if var[2] == sample]
                 sentence_ref = [var[:2] for var in all_ref if var[2] == sample]
                 w.MapDB(sample, sentence_var, sentence_ref)
 
-            TimeStamp("map", s_time)
-
             # Query DB
             w.QueryDB(outname, f"{chromosome}_{pos_start}_{pos_end}")
+
+            print("\n")
+            print(f"[Region] {chromosome}\t{pos_start}\t{pos_end}")
+            summary = SummaryVariants(all_var, uniq_var, samples, w.filter_var)
+            join_ts = TimeStamp("join", st)
+
+            logger.info(
+                f"[Region] {chromosome}\t{pos_start}\t{pos_end}\n{summary}\n{join_ts}\n"
+            )
+
             pbar.update(1)
 
 
@@ -85,6 +92,7 @@ if __name__ == "__main__":
     cfg = ArgConfig().load()
 
     logger = Logger(cfg.raw["outdir"]).run()
+    logger.info(">> Start Processing")
     threads, dataList = ThreadsManager(cfg).main()
 
     with Pool(processes=threads) as pool:
@@ -96,7 +104,7 @@ if __name__ == "__main__":
         )
         monitor_thread.start()
 
-        results = pool.map(main, dataList)
+        results = pool.starmap(main, zip(dataList, [logger * threads]))
 
         stop_flag.set()
         monitor_thread.join()
@@ -107,4 +115,5 @@ if __name__ == "__main__":
             f"[Usage] CPU: {int(result_dict['max_cpu']/100)} cores | MEM: {result_dict['max_mem']:.1f} MB"
         )
 
-    TimeStamp("total", start_time)
+    total_ts = TimeStamp("total", start_time)
+    logger.info(f"{total_ts}")
