7407a6af86f8df531201b5ae1c33b727b1e0e9ca
Moonipur <moo_sutthittha@hotmail.com>
2025-12-11 19:55:19 +0700

    Edit format transform 3

--- src/extract_regions.py
+++ src/extract_regions.py
@@ -19,7 +19,7 @@ def Extract_region(dirpath, chr, start, end, qual, depth, gq):
             for vix, var in enumerate(records):
 
                 # Format must be (GT:GQ:DP:AD:AF:PL)
-                eachVar, Var, PASS = FormatTransform(var[8], var[9])
+                eachVar, PASS = FormatTransform(var[8], var[9])
                 eachVar.append(var[5])
 
                 if not PASS:
